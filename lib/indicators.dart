ddf3ffbe66a3ff03d3fbbdc020e523973eb3fd6e
Ramon Rios <rios.osuna.47@gmail.com>
2022-12-26 14:07:12 -0700

    Se agregaron leyendas en la grafica de barras

---
+++ lib/indicators.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movil/utilities/app_colors.dart';
+
+class Indicator extends StatelessWidget {
+  const Indicator({
+    super.key,
+    required this.color,
+    required this.text,
+    this.size = 15,
+    this.textColor = AppColors.black,
+  });
+  final Color color;
+  final String text;
+  final double size;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          text,
+          style: GoogleFonts.mukta(
+              fontSize: 16,
+              color: AppColors.black,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w600),
+        )
+      ],
+    );
+  }
+}
--- lib/widgets/bar_chart.dart
+++ lib/widgets/bar_chart.dart
@@ -1,6 +1,7 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+import '../indicators.dart';
 import '../utilities/app_colors.dart';
 
 class DetailsChart extends StatelessWidget {
@@ -13,27 +14,49 @@ class DetailsChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2,
-      child: BarChart(BarChartData(
-          borderData: FlBorderData(
-              border: const Border(
-                  top: BorderSide.none,
-                  right: BorderSide.none,
-                  left: BorderSide.none,
-                  bottom: BorderSide(width: 1))),
-          groupsSpace: 2,
-          barGroups: [
-            BarChartGroupData(x: taken.toInt(), barRods: [
-              BarChartRodData(toY: taken, width: 20, color: AppColors.blue),
-            ]),
-            BarChartGroupData(x: untaken.toInt(), barRods: [
-              BarChartRodData(toY: untaken, width: 20, color: AppColors.mainRed)
-            ]),
-            BarChartGroupData(x: remain.toInt(), barRods: [
-              BarChartRodData(toY: remain, width: 20, color: AppColors.gray)
-            ])
-          ])),
+    return Column(
+      children: [
+        AspectRatio(
+          aspectRatio: 2,
+          child: BarChart(BarChartData(
+              borderData: FlBorderData(
+                  border: const Border(
+                      top: BorderSide.none,
+                      right: BorderSide.none,
+                      left: BorderSide.none,
+                      bottom: BorderSide(width: 1))),
+              groupsSpace: 2,
+              barGroups: [
+                BarChartGroupData(x: taken.toInt(), barRods: [
+                  BarChartRodData(toY: taken, width: 20, color: AppColors.blue),
+                ]),
+                BarChartGroupData(x: untaken.toInt(), barRods: [
+                  BarChartRodData(
+                      toY: untaken, width: 20, color: AppColors.mainRed)
+                ]),
+                BarChartGroupData(x: remain.toInt(), barRods: [
+                  BarChartRodData(toY: remain, width: 20, color: AppColors.gray)
+                ])
+              ])),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const <Widget>[
+            Indicator(
+              color: AppColors.blue,
+              text: "Ingerido",
+            ),
+            Indicator(
+              color: AppColors.mainRed,
+              text: "No Ingerido",
+            ),
+            Indicator(
+              color: AppColors.gray,
+              text: "Restantes",
+            )
+          ],
+        ),
+      ],
     );
   }
 }
